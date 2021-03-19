235fad8c520aac4dd2c1fd816ef285bbc4291268
isidora_bukusic <isidora@getadigital.com>
2021-03-19 15:37:36 +0100

    fetch data from sanity

--- serverless-blog-app/pages/index.js
+++ serverless-blog-app/pages/index.js
@@ -4,11 +4,13 @@ import PageLayout from "components/PageLayout";
 import AuthorIntro from "components/AuthorIntro";
 import CardLIstItem from "components/CardListItem";
 import CardItem from "components/CardItem";
+import { getAllBlogs } from "lib/api";
 
-const Home = () => (
+const Home = ({ blogs }) => (
   <PageLayout className="blog-detail-page">
     <AuthorIntro />
     <hr />
+    {JSON.stringify(blogs)}
     <Row className="mb-5">
       <Col md="10">
         <CardLIstItem />
@@ -19,5 +21,13 @@ const Home = () => (
     </Row>
   </PageLayout>
 );
-
 export default Home;
+
+export async function getStaticProps() {
+  const blogs = await getAllBlogs();
+  return {
+    props: {
+      blogs,
+    },
+  };
+}
