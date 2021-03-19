a44b1e19b45ebbb2e4395baa6b6d4002daab2606
isidora_bukusic <isidora@getadigital.com>
2021-03-19 16:35:57 +0100

    show sanity data in app

--- serverless-blog-app/pages/index.js
+++ serverless-blog-app/pages/index.js
@@ -10,14 +10,15 @@ const Home = ({ blogs }) => (
   <PageLayout className="blog-detail-page">
     <AuthorIntro />
     <hr />
-    {JSON.stringify(blogs)}
     <Row className="mb-5">
-      <Col md="10">
+      {/* <Col md="10">
         <CardLIstItem />
-      </Col>
-      <Col md="4">
-        <CardItem />
-      </Col>
+      </Col> */}
+      {blogs.map((blog) => (
+        <Col key={blog.slug} md="4">
+          <CardItem title={blog.title} subtitle={blog.subtitle} />
+        </Col>
+      ))}
     </Row>
   </PageLayout>
 );
