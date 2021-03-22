68abda5e93cc202fd8b5bd950c28cb8f3ab8e26a
isidora_bukusic <isidora@getadigital.com>
2021-03-22 17:34:41 +0100

    making dinamic page static with getStaticPaths

--- serverless-blog-app/pages/blogs/[slug].js
+++ serverless-blog-app/pages/blogs/[slug].js
@@ -9,10 +9,27 @@ const BlogDetail = ({ blog }) => {
   );
 };
 
-export async function getServerSideProps({ params }) {
+export async function getStaticProps({ params }) {
   const blog = await getBlogBySlug(params.slug);
   return {
     props: { blog },
+  };
+}
+
+export function getStaticPaths() {
+  return {
+    paths: [
+      {
+        params: { slug: "my-first-blog" },
+      },
+      {
+        params: { slug: "my-second-blog" },
+      },
+      {
+        params: { slug: "my-third-blog" },
+      },
+    ],
+    fallback: false,
   };
 }
 
